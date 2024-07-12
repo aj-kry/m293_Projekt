4b8daa8d3de1e47205a7b83d75d097588b48dc30
aj-kry <ajnakryeziu09@gmail.com>
2024-07-12 11:53:34 +0200

    commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Bilder Karusell
 let slides;
     let currentSlide = 0;
 
@@ -19,3 +20,29 @@ let slides;
       currentSlide = (n + slides.length) % slides.length;
       slides[currentSlide].classList.add('active');
     }
+
+// Transition Bilder fade-in
+    function isInViewport(element) {
+      const rect = element.getBoundingClientRect();
+      return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+  }
+
+  document.addEventListener('DOMContentLoaded', () =>{
+    const fadeInElements = document.querySelectorAll('.fade-in');
+
+    document.addEventListener('scroll', function(){
+      fadeInElements.forEach(element => {
+        if(isInViewport(element)){
+          element.classList.add("fade-in-active");
+        }
+
+      });
+    })
+
+  })
+    
